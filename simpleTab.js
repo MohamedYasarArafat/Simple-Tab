3f459699dc18730ee0ecb045ca0d9b2d675e9516
MohamedYasarArafat <49508470+MohamedYasarArafat@users.noreply.github.com>
2024-04-23 16:55:40 +0530

    Simple Tab Files Updated

---
+++ simpleTab.js
@@ -0,0 +1,16 @@
+
+//function declaration
+function openCity(evt, cityName) {
+  //variable declaration
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].classList.remove("activetab");
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).classList.add("activetab");
+  evt.currentTarget.className += " active";
+}
